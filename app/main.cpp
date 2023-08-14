e2bdbba39e74e3ab586c8da4a808997012d29a30
Vicente Viera <plantedcoot706@gmail.com>
2023-08-13 19:28:54 -0600

    JUCE Framework Working

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <juce_Colour.h>
+#include <juce_opengl/juce_opengl.h>
 #include "my_lib.h"
 #include "config.hpp"
+
+juce::Slider S;
 
 int main(int argc, char** argv){
     print_hello_world();
     std::cout<<NLOHMANN_JSON_VERSION_MAJOR<<std::endl;
+    std::cout<<S.getBottom();
 
     return 0;
 }
