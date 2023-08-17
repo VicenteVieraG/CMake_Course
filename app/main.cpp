1fb1aa5847d125e24a0bfe4932fa72ca3a1cfdfb
Vicente Viera <plantedcoot706@gmail.com>
2023-08-17 16:00:16 -0600

    Included Libraries via FetchContent Works! :D

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <nlohmann/json.hpp>
 #include <my_lib.h>
-#include <juce_core.h>
-#include <juce_opengl.h>
 #include "config.hpp"
-
-juce::URL owo(juce::File(""));
 
 int main(int argc, char** argv){
     print_hello_world();
-    std::cout<<NLOHMANN_JSON_VERSION_MAJOR<<std::endl;
+    
     return 0;
 }
--- src/my_lib/my_lib.cpp
+++ src/my_lib/my_lib.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <fmt/format.h>
+#include <cxxopts.hpp>
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
 
 void print_hello_world(){
     std::cout<<"Hola Crayola"<<std::endl;
+
+    std::cout<<"NLOHMANN_JSON:"<<std::endl;
+    std::cout<<NLOHMANN_JSON_VERSION_MAJOR<<std::endl;
+    std::cout<<NLOHMANN_JSON_VERSION_MINOR<<std::endl;
+    std::cout<<NLOHMANN_JSON_VERSION_PATCH<<std::endl;
+
+    std::cout<<"FMT:"<<std::endl;
+    std::cout<<FMT_VERSION<<std::endl;
+    
+    std::cout<<"SPDLOG:"<<std::endl;
+    std::cout<<SPDLOG_VERSION<<std::endl;
+
+    std::cout<<"CXXOPTS:"<<std::endl;
+    std::cout<<CXXOPTS__VERSION_MAJOR<<"."<<CXXOPTS__VERSION_MINOR<<"."<<CXXOPTS__VERSION_PATCH<<std::endl;
 }
