a91adf9e3bd45fdb80d099a681ebd424b3f9e2d4
Vicente Viera <plantedcoot706@gmail.com>
2023-08-17 12:57:05 -0600

    Dependencies Working

--- app/main.cpp
+++ app/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
-#include <juce_Colour.h>
-#include <juce_opengl/juce_opengl.h>
+#include <juce_core.h>
+#include <juce_opengl.h>
 #include "my_lib.h"
 #include "config.hpp"
 
-juce::Slider S;
+juce::URL owo(juce::File(""));
 
 int main(int argc, char** argv){
     print_hello_world();
     std::cout<<NLOHMANN_JSON_VERSION_MAJOR<<std::endl;
-    std::cout<<S.getBottom();
-
     return 0;
 }
